cb3f0be86146d30ff76a003edb4477abfbb946e4
RemainingToast <Remainingtoast@gmail.com>
2022-10-27 06:42:40 +1000

    start on multi version base

---
+++ toastclient-mc19-fabric/src/main/java/me/remainingtoast/toastclient/client/ToastClientClient.java
@@ -0,0 +1,13 @@
+package me.remainingtoast.toastclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ToastClientClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
