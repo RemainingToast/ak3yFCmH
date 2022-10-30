402469ab137e6780833e0f0d90b601b09eaedcb4
RemainingToast <Remainingtoast@gmail.com>
2022-10-31 04:55:20 +1000

    fix fabric module

--- toastclient-mc19-fabric/src/main/java/me/remainingtoast/toastclient/ToastClient.java
+++ toastclient-mc19-fabric/src/main/java/me/remainingtoast/toastclient/ToastClient.java
@@ -1,13 +1,25 @@
 package me.remainingtoast.toastclient;
 
+import me.remainingtoast.toastclient.api.IToastClient;
+import me.remainingtoast.toastclient.api.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class ToastClient implements ClientModInitializer {
+public class ToastClient implements ClientModInitializer, IToastClient {
     @Override
     public void onInitializeClient() {
 
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public ModuleManager getModuleManager() {
+        return new ModuleManager(this);
     }
 }
