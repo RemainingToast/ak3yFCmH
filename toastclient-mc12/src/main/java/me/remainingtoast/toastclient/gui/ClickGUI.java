8f804f15a579cb58976006e834b38f105679bd58
RemainingToast <Remainingtoast@gmail.com>
2022-10-31 05:44:47 +1000

    add git commit to jar

---
+++ toastclient-mc12/src/main/java/me/remainingtoast/toastclient/gui/ClickGUI.java
@@ -0,0 +1,21 @@
+package me.remainingtoast.toastclient.gui;
+
+import com.lukflug.panelstudio.hud.HUDGUI;
+import com.lukflug.panelstudio.mc12.MinecraftHUDGUI;
+
+public class ClickGUI extends MinecraftHUDGUI {
+    @Override
+    protected HUDGUI getGUI() {
+        return null;
+    }
+
+    @Override
+    protected GUIInterface getInterface() {
+        return null;
+    }
+
+    @Override
+    protected int getScrollSpeed() {
+        return 0;
+    }
+}
