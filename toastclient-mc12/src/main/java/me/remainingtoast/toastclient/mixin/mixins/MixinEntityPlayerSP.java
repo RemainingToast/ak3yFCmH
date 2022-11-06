d9ec1c5e6b189f009d6a94b92ce1aa850cca02e9
RemainingToast <Remainingtoast@gmail.com>
2022-11-06 23:36:40 +1000

    remove api to run

--- toastclient-mc12/src/main/java/me/remainingtoast/toastclient/mixin/mixins/MixinEntityPlayerSP.java
+++ toastclient-mc12/src/main/java/me/remainingtoast/toastclient/mixin/mixins/MixinEntityPlayerSP.java
@@ -1,7 +1,5 @@
 package me.remainingtoast.toastclient.mixin.mixins;
 
-import me.remainingtoast.toastclient.ToastClient;
-import me.remainingtoast.toastclient.api.events.PlayerTickEvent;
 import net.minecraft.client.entity.EntityPlayerSP;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,13 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinEntityPlayerSP {
     @Inject(method = "onUpdate", at = @At("HEAD"))
     public void preUpdate(CallbackInfo ci) {
-        PlayerTickEvent.Pre event = new PlayerTickEvent.Pre();
-        ToastClient.eventBus.post(event);
+//        PlayerTickEvent.Pre event = new PlayerTickEvent.Pre();
+//        ToastClient.eventBus.post(event);
     }
 
     @Inject(method = "onUpdate", at = @At("TAIL"))
     public void postUpdate(CallbackInfo ci) {
-        PlayerTickEvent.Post event = new PlayerTickEvent.Post();
-        ToastClient.eventBus.post(event);
+//        PlayerTickEvent.Post event = new PlayerTickEvent.Post();
+//        ToastClient.eventBus.post(event);
     }
 }
